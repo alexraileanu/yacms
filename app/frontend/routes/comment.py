e524e0a13b64bcacff7a852b18ddaaaf6975124b
Alex <alexraileanu@me.com>
2017-09-25 16:06:29 +0200

    changed fetching of obj to the new way

--- app/frontend/routes/comment.py
+++ app/frontend/routes/comment.py
@@ -1,11 +1,9 @@
-from app.backend import db
 from app.frontend import frontend
 from app.backend.models.comment import Comment
 from app.backend.models.article import Article
 from app.frontend.forms.comment import CommentForm
 from flask import request, flash, redirect
 from flask_user import current_user
-from sqlalchemy.exc import SQLAlchemyError
 
 
 @frontend.route('/comment/add', methods=['POST'])
@@ -18,18 +16,12 @@ def comment():
         if current_user.is_authenticated:
             current_user.comments.append(obj)
 
-        article = Article.query.get(form.article_id.data)
+        article = Article.get('id', form.article_id.data)
 
         form.populate_obj(obj)
         article.comments.append(obj)
 
         msg, cat = obj.save()
         flash(msg, cat)
-
-        return redirect(request.referrer)
-
-    if form.erros:
-        for error in form.errors:
-            flash(error, 'error')
 
     return redirect(request.referrer)
